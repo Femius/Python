a0c830d4ab8bd866eceaa035342afd291986ce73
Femius <pagindm@gmail.com>
2025-04-26 15:05:19 +0500

    Update my.py

--- my.py
+++ my.py
@@ -1,38 +1,128 @@
-print('Dmitry')
+# print('Dmitry')
+#
+# print('Вторая строка')
+#
+# print('Третья строка')
+#
+# print('Привет', 'мир', '!')
+#
+# print(55, 67, 78.5, True, False)
+#
+# a = 5
+# b = 10
+# print(a + b)
+# print('5 + 10 =', a + b)
+# print('5 + 10 =', 5 + 10)
+#
+# print('4', '5', '6', '7', sep='-')
+#
+# a = 17
+# print(a)
+# a = b = f = 7
+# print(a, b, f)
+#
+# a -= 1
+# a = a - 1
+# print(a)
+#
+# print(5/2)
+# print(5//2)
+#
+# print(5 * 2)
+# print(5**2)
+#
+# a = 4
+# b = 10
+# print('a =', a, 'b=', b)
+# a, b = b, a
+# print('a =', a, 'b=', b)
+# ввод значения от пользователя в целое число
+a = int (input('Введитевторое число: ')) # ввод значения от пользователя в целое число
+b = int (input('Введите второе число: '))
+print('a =', a) # вывод переменной а
+print('b =', b) # вывод переменной b
+print('Сумма a+b=', a+b) # вывод суммы двух переменных (а + b)
+#
+# '''
+# Третье занятие
+# Python
+# '''
 
-print('Вторая строка')
+# 500_000_000 _ подчеркивание для удобства чтения
+# Целые числа (int)
+# a = 5
+# b = 0
+# c = -10
+# d = 500_000
+# print(a, b, c, d)
+#
+# # дробные числа (float)
+# a = 3.5
+# b = 0.0
+# c = -5.6
+# d = 50_000.000_03
+# e = 4.5e2 # e2 это умножить на 10 в степени 2
+# print(a, b, c, d, e)
 
-print('Третья строка')
+# # лигический тип данных (boolean)
+# print(bool(0))
+# print(bool(-1))
+# print(bool(5))
+# print(bool(0.0))
+# print(bool(True))
+# print(bool(False))
+# print(bool(''))
+# print(bool(' '))
+# print(bool('Привет мир'))
 
-print('Привет', 'мир', '!')
+# print(2 == 2)
+# print(2 == '2')
+# print(2 == "2")
+# print(4 > 2)
+# print(5 >= 5)
+# print(10 <= 5)
+# x = 5
+# a = 1
+# b = 10
+# print(a > x < b)
+#
+# # Строки (string)
+# print("Строка в двойных кавычках")
+# print('Строка в одиночных кавычках')
+# print("М.Ю. Лермонтов 'Бородино'")
+# print("М.Ю. Лермонтов \"Бородино\" ")
+# print("fshsfhgfhfdghfghfgdhfdghfgdhfg"
+#       "hfgdhfg
+#       dhgfhfgdhf"
+#       "gdhfgdhfg
+#       hgfhfgdhfgdh\n" #перенос строки \n
+#       "fgdhfgdhfg"
+#       "dhfghfgdh")
 
-print(55, 67, 78.5, True, False)
+# print("Привет " + " мир" + "!") # конкатенация
+# print("5" + "5")
+# print(5 + 5)
+# # print(5 + "5") # будет ошибка по типу данных
+# print(5 + int("5"))
+#
+#
+# print(5 * 15)
+# print("5" * 3) # дупликат
+# print("Привет " * 3)
+#
+# a = 5
+# b =10
+# result = a + b
+# print(a, "+", b, "=", result)
+# final = str (a) + "+" + str (b) + "=" + str(result)
+# print(final)
+# Final_v2 = (f"Результат выражения:\n"
+#             f"{a} + {b} = {a + b}")
 
-a = 5
-b = 10
-print(a + b)
-print('5 + 10 =', a + b)
-print('5 + 10 =', 5 + 10)
-
-print('4', '5', '6', '7', sep='-')
-
-a = 17
-print(a)
-a = b = f = 7
-print(a, b, f)
-
-a -= 1
-a = a - 1
-print(a)
-
-print(5/2)
-print(5//2)
-
-print(5 * 2)
-print(5**2)
-
-a = 4
-b = 10
-print('a =', a, 'b=', b)
-a, b = b, a
-print('a =', a, 'b=', b)
+# # Списки (list)
+# myList =["Строка", 567, 78.5, True]
+# print(myList)
+# print(myList[0])
+# print(myList[3])
+# print(myList[1:3])
+# print(myList[1:])
