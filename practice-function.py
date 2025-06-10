c6362f8f8f43d4d7853dafeca0137d9b6337c9f7
Femius <pagindm@gmail.com>
2025-06-10 10:31:10 +0500

    Update my.py

---
+++ practice-function.py
@@ -0,0 +1,61 @@
+# name = input("Введите ваше имя: ")
+# def greeting():
+#     print("-----")
+#     print(f"Привет {name}, добро пожаловать")
+#     print("------")
+#
+# greeting()
+#
+# print(name)
+#
+# def f():
+#     global a
+#     global b
+#     b, c = a, b
+# def g():
+#     global a
+#     global b
+#     c = '0'
+#     a = d + c
+# a = '2'
+# b = '3'
+# c = '5'
+# d = '7'
+# f()
+# g()
+# f()
+# print(a + b + c + d)
+
+# # Задача 1
+# def print_lines(n):
+#     print("-" * n)
+#     print("-" * n)
+#
+# n = int(input("Введите количество цепей"))
+# print_lines(n)
+
+# # Задача 2
+# def print_lines(n):
+#     if n < 2:
+#         print("Ширина не менее 2")
+#     print("-" * n)
+#     print("-" + " " * (n-2) + "-")
+#     print("-" * n)
+#
+# n = int(input("Введите количество цепей"))
+# print_lines(n)
+
+# Задача 3
+# def print_tringle(n):
+#     for i in range(1, n + 1):
+#         print("*" * i)
+# n = int(input("Введите количество звезд"))
+# print_tringle(n)
+
+
+
+
+
+
+
+
