fe5b5fb245f92de06812f418154a5d6394bf24b0
Femius <pagindm@gmail.com>
2025-06-14 13:28:53 +0500

    Update my.py

--- practice-function.py
+++ practice-function.py
@@ -51,6 +51,68 @@
 #         print("*" * i)
 # n = int(input("Введите количество звезд"))
 # print_tringle(n)
+# Задача 7
+def distance(x1, y1, x2, y2):
+    return (((x2 - x1)**2 +(y2 - y1)**2))**0.5
+
+def triangle_perimetr(x1, y1, x2, y2, x3, y3):
+    a = distance(x1, y1, x2, y2)
+    b = distance(x2, y2, x3, y3)
+    c = distance(x3, y3 ,x1, y1)
+    return a + b + c
+print(triangle_perimetr(1, 2, 4, 5,6 ,7))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # задача 8 найдите простое трехзначтное число
+# def is_prime(n):
+#     if n < 2: return False
+#     for i in range(2, n):
+#         if n % i == 0:
+#             return False
+#     return True
+# primes = []
+# for i in range(100, 1000):
+#     if is_prime(i):
+#         primes.append(i)
+# print(primes)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
